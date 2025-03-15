36d27797c26df2c91625189dac6567a8f6bbbf72
Danail <d_krasimirov@yahoo.com>
2025-03-15 23:00:53 +0100

    adding toml

--- go-llm-playground/text-corrector/python-web/web.py
+++ go-llm-playground/text-corrector/python-web/web.py
@@ -1,28 +1,32 @@
 import gradio as gr
 import requests
 
+
 def query_golang_app(text):
     # Send request to Golang app
     url = "http://localhost:3000/generate"
     payload = {"prompt": text}
-    
-    try:
-        response = requests.post(url, json=payload)
 
+    try:
+        response = requests.post(url, json=payload, timeout=20)
         if response.ok:  # Checks for any 2xx status code
             return response.json().get("generated_text", "No correction generated.")
-        else:
-            return f"Error: {response.status_code} - {response.text}"
+
+        return f"Error: {response.status_code} - {response.text}"
 
     except requests.exceptions.RequestException as e:
         return f"Request failed: {e}"
+
+
 # Define Gradio interface
 interface = gr.Interface(
     fn=query_golang_app,
-    inputs=gr.Textbox(lines=5,placeholder="Enter text with grammer or spelling mistakes"),
+    inputs=gr.Textbox(
+        lines=5, placeholder="Enter text with grammer or spelling mistakes"
+    ),
     outputs=gr.Textbox(label="Corrected text"),
     title="AI powered Grammer and Spell Checker",
-    description="Enter a prompt to get a response from Ollama via a Golang backend."
+    description="Enter a prompt to get a response from Ollama via a Golang backend.",
 )
 
 if __name__ == "__main__":
